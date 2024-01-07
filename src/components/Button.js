9766ca210dafb0a16cb6ce33d22e08010f06c6a5
Lorenzo Lezcano <lorenzodlezcano@gmail.com>
2024-01-07 19:48:23 -0300

    Project completed

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../stylesheets/Button.css';
+
+
+function Button({ text, isClickButton,  handleClick }) {
+    return (
+        <button className={isClickButton ? 'click-button' : 'restart-button'} onClick={handleClick}>
+            {text}
+        </button>
+    );
+}
+
+export default Button;
